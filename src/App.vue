5c4b0093d5f02eb0d3f5c4f91db80e6958da57d8
daniloeberli <danilo.eberli@gmail.com>
2023-05-25 16:16:36 +0200

    feat: milestone 4

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,45 @@
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
+import AppMain from './components/AppMain.vue';
 
 export default {
     name: 'App',
     data() {
         return {
-            
+            apiBaseUrl: 'http://127.0.0.1:8000/api',
+            apiUrls: {
+                projects: '/projects'
+            },
+            projects: []
         }
     },
     components: {
         AppHeader,
-        AppFooter
+        AppFooter,
+        AppMain
+    },
+    methods: {
+        getProjects() {
+            axios.get(this.apiBaseUrl + this.apiUrls.projects)
+                 .then((response) => {
+                    //console.log(response);
+                    this.projects = response.data.results;
+                 })
+                 .catch((error) => {
+                    console.error(error);
+                 })
+        }
+    },
+    created(){
+        this.getProjects();
     }
 }
 </script>
 
 <template>
     <AppHeader></AppHeader>
+    <AppMain></AppMain>
     <AppFooter></AppFooter>
 </template>
 
